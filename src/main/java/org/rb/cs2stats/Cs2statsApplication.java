2eeba22bdbcfb0643287364fe9b4a59373290365
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-12-12 10:09:13 +0100

    First commit

---
+++ src/main/java/org/rb/cs2stats/Cs2statsApplication.java
@@ -0,0 +1,13 @@
+package org.rb.cs2stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2statsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2statsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rb/cs2stats/Cs2statsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rb.cs2stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2statsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
